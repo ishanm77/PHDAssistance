e92abc2b2301d12e93cb7d3faed1b1e2ab67485f
Ishan Mishra <ishanmishra303@gmail.com>
2024-06-13 09:05:53 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 4,
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
